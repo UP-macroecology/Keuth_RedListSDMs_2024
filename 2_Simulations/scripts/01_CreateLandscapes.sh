f698c88f2fcd731e47a541d135ba709c6e89dc17
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2024-05-22 11:51:16 +0200

    updated folder structure

--- 2_Simulations/scripts/01_CreateLandscapes.sh
+++ 2_Simulations/scripts/01_CreateLandscapes.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=landscapes_cc
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=keuth@uni-potsdam.de
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=20
 #SBATCH --time=96:00:00
 #SBATCH --mem=150gb
-#SBATCH --nodelist=ecoc9z
+#SBATCH --nodelist=ecoc9
 
 cd ${SLURM_SUBMIT_DIR}
 R CMD BATCH ./01_CreateLandscapes.R ./output-file-landscapes_cc.Rout
