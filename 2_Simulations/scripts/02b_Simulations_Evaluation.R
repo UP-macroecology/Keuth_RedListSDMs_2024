c46ab430cb03ef1a8fafaa1a32766f011c55de9f
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2024-05-29 15:08:40 +0200

    initial commit

---
+++ 2_Simulations/scripts/02b_Simulations_Evaluation.R
@@ -0,0 +1,97 @@
+# Evaluating simulation results
+
+# Load packages
+library(dplyr)
+library(data.table)
+library(ggplot2)
+library(gridExtra)
+
+# Load functions
+source("2_Simulations/scripts/text_labels_plots.R")
+source("Functions/extract_legend.R")
+
+optima <- c("cold", "warm")
+breadth <- c("narrow", "wide")
+rmax <- c("slow", "fast")
+dispersal <- c("short", "long")
+
+sims <- expand.grid(optima = optima, breadth = breadth, rmax = rmax, dispersal = dispersal)
+
+# Plot abundance for the different landscapes and scenarios
+abundance <- vector("list", 16)
+for (i in 1:16) {
+  tmp <- readRDS(paste0("2_Simulations/data/SumInd_Batch", i, "_Sim1.rds"))
+  tmp$scenario <- i #add scenario to respective data set
+  tmp$optima <- sims[i,]$optima
+  tmp$breadth <- sims[i,]$breadth
+  tmp$rmax <- sims[i,]$rmax
+  tmp$dispersal <- sims[i,]$dispersal
+  abundance[[i]] <- tmp
+  rm(tmp)
+}
+
+#remove years of spin up and without population
+abundance <- lapply(abundance, function(x){x <- x[!x$Year %in% c(0:97),];
+                    x$scenario <- as.factor(x$scenario);
+                    x$optima <- as.factor(x$optima);
+                    x$breadth <- as.factor(x$breadth);
+                    x$rmax <- as.factor(x$rmax);
+                    x$dispersal <- as.factor(x$dispersal);
+                    return(x)})
+
+#calculate mean and sd over all replications, reduce Year so year 100 is 0
+abundance <- lapply(abundance, function(x){x <- x %>% group_by(Year, scenario, optima, breadth, rmax, dispersal) %>% summarise(meanPop = mean(sumPop), sdPop = sd(sumPop), 
+                                                                                       .groups = "keep");
+                                      x <- as.data.frame(x); 
+                                      x$Year <- x$Year - 100; return(x)})
+
+a1 <- ggplot(abundance[[1]], aes(x = Year, y = meanPop, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = abundance[[5]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[9]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[13]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("mean Abundance"))+
+  theme(panel.grid = element_blank(), panel.background = element_rect(fill = "white"), panel.border = element_rect(colour = "black", fill = NA), 
+        legend.position = "", axis.title.x = element_blank(), axis.title = element_text(size = 23), axis.text = element_text(size = 20))
+
+a2 <- ggplot(abundance[[3]], aes(x = Year, y = meanPop, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = abundance[[7]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[11]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[15]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("mean Abundance"))+
+  theme(panel.grid = element_blank(), panel.background = element_rect(fill = "white"), panel.border = element_rect(colour = "black", fill = NA), 
+        legend.position = "", axis.title.x = element_blank(), axis.title = element_text(size = 23), axis.text = element_text(size = 20))
+
+a3 <- ggplot(abundance[[2]], aes(x = Year, y = meanPop, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = abundance[[6]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[10]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[14]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("mean Abundance"))+
+  theme(panel.grid = element_blank(), panel.background = element_rect(fill = "white"), panel.border = element_rect(colour = "black", fill = NA), 
+        legend.position = "", axis.title.x = element_blank(), axis.title = element_text(size = 23), axis.text = element_text(size = 20))
+
+a4 <- ggplot(abundance[[4]], aes(x = Year, y = meanPop, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = abundance[[8]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[12]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[16]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("mean Abundance"))+
+  theme(panel.grid = element_blank(), panel.background = element_rect(fill = "white"), panel.border = element_rect(colour = "black", fill = NA), 
+        legend.position = "", axis.title.x = element_blank(), axis.title = element_text(size = 23), axis.text = element_text(size = 20))
+
+legend <- ggplot(abundance[[4]], aes(x = Year, y = meanPop, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = abundance[[8]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[12]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = abundance[[16]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("mean Abundance"))+
+  theme(legend.spacing.y = unit(0.5, 'cm'), legend.title=element_text(size = 23), legend.text = element_text(size = 19), legend.position = "bottom", 
+        legend.key.size = unit(1, "cm"))
+
+shared_legend <- extract_legend(legend)
+
+# Draw plots with shared legend
+g <- grid.arrange(arrangeGrob(t0,t_c_medium, t_w_medium, t_nn, a1, a3, t_wn, a2, a4, ncol = 3, nrow = 3, heights= c(1,8,8), widths = c(1,5.5,5.5)),
+                  t0,shared_legend, nrow = 2, ncol = 2, heights = c(11.2, 0.8), widths = c(11.7,0.3))
---
+++ 2_Simulations/scripts/text_labels_plots.R
@@ -0,0 +1,332 @@
+# text labels for grid.arrange graphs
+library(ggplot2)
+
+# empty plot for the upper left corner of the plots
+t0 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "") +
+  theme_void()
+
+# text plots for niche widths
+t_nn <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 8,
+           label = "narrow \nniche") +
+  theme_void()
+
+t_wn <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 8,
+           label = "wide \nniche") +
+  theme_void()
+
+#text plots for niche optima
+t_c_small <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 6.5,
+           label = "cold-adapted") +
+  theme_void()
+
+t_w_small <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 6.5,
+           label = "warm-adapted") +
+  theme_void()
+
+t_c_medium <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 8,
+           label = "cold-adapted") +
+  theme_void()
+
+t_w_medium <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 8,
+           label = "warm-adapted") +
+  theme_void()
+
+t_c_large <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 10,
+           label = "cold-adapted") +
+  theme_void()
+
+t_w_large <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 10,
+           label = "warm-adapted") +
+  theme_void()
+
+# text plots for years
+t_year0 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 7,
+           label = "Year 0") +
+  theme_void()
+
+t_year10 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 7,
+           label = "Year 10") +
+  theme_void()
+
+t_year20 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 7,
+           label = "Year 20") +
+  theme_void()
+
+t_year30 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 7,
+           label = "Year 30") +
+  theme_void()
+
+#label for scenarios
+l1 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted\nnarrow niche\nslow gr. rate \nshort disp.") +
+  theme_void()
+
+l2 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted\nnarrow niche\nslow gr. rate \nlong disp.") +
+  theme_void()
+
+l3 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted\nnarrow niche\nfast gr. rate \nshort disp.") +
+  theme_void()
+
+l4 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted\nnarrow niche\nfast gr. rate \nlong disp.") +
+  theme_void()
+
+l5 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted\nwide niche\nslow gr. rate \nshort disp.") +
+  theme_void()
+
+l6 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted\nwide niche\nslow gr. rate \nlong disp.") +
+  theme_void()
+
+l7 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted\nwide niche\nfast gr. rate \nshort disp.") +
+  theme_void()
+
+l8 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted\nwide niche\nfast gr. rate \nlong disp.") +
+  theme_void()
+
+l9 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted\nnarrow niche\nslow gr. rate \nshort disp.") +
+  theme_void()
+
+l10 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted\nnarrow niche\nslow gr. rate \nlong disp.") +
+  theme_void()
+
+l11 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted\nnarrow niche\nfast gr. rate \nshort disp.") +
+  theme_void()
+
+l12 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted\nnarrow niche\nfast gr. rate \nlong disp.") +
+  theme_void()
+
+l13 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted\nwide niche\nslow gr. rate \nshort disp.") +
+  theme_void()
+
+l14 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted\nwide niche\nslow gr. rate \nlong disp.") +
+  theme_void()
+
+l15 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted\nwide niche\nfast gr. rate \nshort disp.") +
+  theme_void()
+
+l16 <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted\nwide niche\nfast gr. rate \nlong disp.") +
+  theme_void()
+
+# dispersal growth rate combinations
+
+t_ss <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "slow growth rate & short dispersal distance") +
+  theme_void()
+
+t_sl <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "slow growth rate & long dispersal distance") +
+  theme_void()
+
+t_fs <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "fast growth rate & short dispersal distance") +
+  theme_void()
+
+t_fl <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "fast growth rate & long dispersal distance") +
+  theme_void()
+
+# niche combinations
+
+t_cn <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted &\nnarrow niche") +
+  theme_void()
+
+t_cw <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "cold-adapted &\nwide niche") +
+  theme_void()
+
+t_wna <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted &\nnarrow niche") +
+  theme_void()
+
+t_ww <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 4,
+           label = "warm-adapted &\nwide niche") +
+  theme_void()
+
+# threatened values
+t_VU <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 8,
+           label = "Vulnerable") +
+  theme_void()
+
+t_EN <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 8,
+           label = "Endangered") +
+  theme_void()
+
+t_CR <- ggplot() +
+  annotate("text",
+           x = 1,
+           y = 1,
+           size = 8,
+           label = "Critically endangered") +
+  theme_void()
