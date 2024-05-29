b890b70d5993bf46e56614823b2f94bb9251c5c4
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2024-05-29 15:09:10 +0200

    added new graphs

--- 2_Simulations/scripts/02b_Simulations_Evaluation.R
+++ 2_Simulations/scripts/02b_Simulations_Evaluation.R
@@ -20,7 +20,7 @@ sims <- expand.grid(optima = optima, breadth = breadth, rmax = rmax, dispersal =
 # Plot abundance for the different landscapes and scenarios
 abundance <- vector("list", 16)
 for (i in 1:16) {
-  tmp <- readRDS(paste0("2_Simulations/data/SumInd_Batch", i, "_Sim1.rds"))
+  tmp <- readRDS(paste0("2_Simulations/data/SumInd_Batch", i, "_Sim3.rds"))
   tmp$scenario <- i #add scenario to respective data set
   tmp$optima <- sims[i,]$optima
   tmp$breadth <- sims[i,]$breadth
@@ -94,4 +94,75 @@ shared_legend <- extract_legend(legend)
 
 # Draw plots with shared legend
 g <- grid.arrange(arrangeGrob(t0,t_c_medium, t_w_medium, t_nn, a1, a3, t_wn, a2, a4, ncol = 3, nrow = 3, heights= c(1,8,8), widths = c(1,5.5,5.5)),
+                  t0,shared_legend, nrow = 2, ncol = 2, heights = c(11.2, 0.8), widths = c(11.7,0.3))
+
+# Extinction plots
+extinction <- vector("list", 16)
+for (i in 1:16) {
+  tmp <- readRDS(paste0("2_Simulations/data/ExtProb_Batch", i, "_Sim3.rds"))
+  tmp$scenario <- i #add scenario to the data set
+  tmp$scenario <- as.factor(tmp$scenario)
+  tmp$Year <- tmp$Year -100
+  tmp$optima <- sims[i,]$optima
+  tmp$breadth <- sims[i,]$breadth
+  tmp$rmax <- sims[i,]$rmax
+  tmp$dispersal <- sims[i,]$dispersal
+  extinction[[i]] <- tmp
+  rm(tmp)
+}
+
+
+e1 <- ggplot(extinction[[1]], aes(x = Year, y = extProb, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = extinction[[5]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[9]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[13]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("Extinction probability"))+
+  xlim(-3,80)+
+  theme(panel.grid = element_blank(), panel.background = element_rect(fill = "white"), panel.border = element_rect(colour = "black", fill = NA), 
+        legend.position = "", axis.title.x = element_blank(), axis.title = element_text(size = 23), axis.text = element_text(size = 20))
+
+e2 <- ggplot(extinction[[3]], aes(x = Year, y = extProb, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = extinction[[7]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[11]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[15]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("Extinction probability"))+
+  xlim(-3,80)+
+  theme(panel.grid = element_blank(), panel.background = element_rect(fill = "white"), panel.border = element_rect(colour = "black", fill = NA), 
+        legend.position = "", axis.title.x = element_blank(), axis.title = element_text(size = 23), axis.text = element_text(size = 20))
+
+e3 <- ggplot(extinction[[2]], aes(x = Year, y = extProb, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = extinction[[6]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[10]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[14]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("Extinction probability"))+
+  xlim(-3,80)+
+  theme(panel.grid = element_blank(), panel.background = element_rect(fill = "white"), panel.border = element_rect(colour = "black", fill = NA), 
+        legend.position = "", axis.title.x = element_blank(), axis.title = element_text(size = 23), axis.text = element_text(size = 20))
+
+e4 <- ggplot(extinction[[4]], aes(x = Year, y = extProb, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = extinction[[8]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[12]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[16]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("Extinction probability"))+
+  xlim(-3,80)+
+  theme(panel.grid = element_blank(), panel.background = element_rect(fill = "white"), panel.border = element_rect(colour = "black", fill = NA), 
+        legend.position = "", axis.title.x = element_blank(), axis.title = element_text(size = 23), axis.text = element_text(size = 20))
+
+legend <- ggplot(extinction[[4]], aes(x = Year, y = extProb, linetype = rmax, colour = dispersal))+
+  geom_line(linewidth = 1.8)+
+  geom_line(data = extinction[[8]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[12]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  geom_line(data = extinction[[16]], aes(linetype = rmax, colour = dispersal), linewidth = 1.8)+
+  ylab(c("mean extinction"))+
+  theme(legend.spacing.y = unit(0.5, 'cm'), legend.title=element_text(size = 23), legend.text = element_text(size = 19), legend.position = "bottom", 
+        legend.key.size = unit(1, "cm"))
+
+shared_legend <- extract_legend(legend)
+
+# Draw plots with shared legend
+g <- grid.arrange(arrangeGrob(t0,t_c_medium, t_w_medium, t_nn, e1, e3, t_wn, e2, e4, ncol = 3, nrow = 3, heights= c(1,8,8), widths = c(1,5.5,5.5)),
                   t0,shared_legend, nrow = 2, ncol = 2, heights = c(11.2, 0.8), widths = c(11.7,0.3))
