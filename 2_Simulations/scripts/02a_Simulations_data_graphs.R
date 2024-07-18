9c38dff1c5fb1253fbed66d18add9a9022aeefeb
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2024-07-18 14:24:28 +0200

    added coordinate correction

--- 2_Simulations/scripts/02a_Simulations_data_graphs.R
+++ 2_Simulations/scripts/02a_Simulations_data_graphs.R
@@ -35,11 +35,11 @@ dispersal <- c(5000, 15000)
 sims <- expand.grid(land_rep = land_rep, optima = optima, breadth = breadth, rmax = rmax, dispersal = dispersal)
 sims$BatchNum <- rep(1:16, each = 3)
 
-ncores <- 1
+ncores <- 5
 cl <- makeCluster(ncores)
 registerDoParallel(cl)
 
-foreach(sim_nr=46:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "tibble", "scales", "ggplot2", "gridExtra", "terra", "data.table")) %dopar% {
+foreach(sim_nr=1:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "tibble", "scales", "ggplot2", "gridExtra", "terra", "data.table")) %dopar% {
   # Extract parameter values
   rep_nr <- sims[sim_nr,]$land_rep
   optima <- sims[sim_nr,]$optima
@@ -51,30 +51,30 @@ foreach(sim_nr=46:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "t
   # Set up the dynamic landscapes ------------------------------------------------------------------------------------
   # Numbers of spinup years in dynamic landscape
   spinup <- 100
-  
+
   # Set dynamic landscape parameters
   landnames <- c()
   for (i in 0:89){
     landnames <- append(landnames, paste0("land", rep_nr, "_optima",  optima, "_breadth", breadth, "_ccYear", i, ".asc"))
   }
-  
+
   #create vector for the years (landscapes start changing at year 101)
   years <- seq(spinup+1, length(landnames)+99,1)
   years <- c("0", years)
   years  <- as.numeric(years)
-  
+
   # Define Landscape module ------------------------------------------------------------------------------------
-  
+
   land <- ImportedLandscape(LandscapeFile = landnames,
                             DynamicLandYears = years,
                             Resolution = 1000,
                             HabPercent = TRUE,
                             K_or_DensDep = 0.05)
-  
+
   # Define demography module ------------------------------------------------------------------------------------
-  
+
   demo <- Demography(Rmax = rmax, ReproductionType = 0) # asexual model with no stage structure
-  
+
   # Define dispersal module ------------------------------------------------------------------------------------
   if(dispersal == 5000){
     disp <-  Dispersal(
@@ -95,12 +95,12 @@ foreach(sim_nr=46:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "t
       Settlement = Settlement(Settle = 2)
     )
   }
-  
+
   # Define initial conditions for simulations ------------------------------------------------------------------------------------
-  
+
   # Initialisation in all suitable cells at half carrying capacity
   init <- Initialise(InitType = 0, FreeType = 1, InitDens = 1)
-  
+
   RepNb <- 100 #100 replicated runs
   sim_years <- 90 #let simulation run for 90 years
   sim <- Simulation(Simulation = rep_nr,
@@ -108,32 +108,36 @@ foreach(sim_nr=46:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "t
                     Years = spinup + sim_years,
                     OutIntPop = 1,
                     OutIntOcc = 1)
-  
+
   s <- RSsim(batchnum = BatchNum, land = land, demog = demo, dispersal = disp, simul = sim, init = init)
   
   # Calculation of different values  -------------------------------------------------------------------
   
   # Calculate extinction probability
-  #pop <- read.table(paste0(sim_dir, "Outputs/Batch", BatchNum, "_Sim", rep_nr, "_Land1_Pop.txt"), sep = ",", header = T)
   pop <- fread(paste0(sim_dir, "Outputs/Batch", BatchNum, "_Sim", rep_nr, "_Land1_Pop.txt"))
-  extProb <- Calc_ExtProb(pop, s) 
-  saveRDS(extProb, paste0(sim_dir, file = "Outputs/ExtProb_Batch", BatchNum, "_Sim", rep_nr, ".rds"))
   
+  extProb <- Calc_ExtProb(pop, s)
+  saveRDS(extProb, paste0(sim_dir, file = "Outputs/ExtProb_Batch", BatchNum, "_Sim", rep_nr, ".rds"))
+
   # Calculate maximum number of individuals per replicate run and year
   sumInd_s <- pop %>% group_by(Rep,Year) %>% summarise(sumPop = sum(NInd), .groups='keep')
-  
+
   saveRDS(sumInd_s, file = paste0(sim_dir, "Outputs/SumInd_Batch", BatchNum, "_Sim", rep_nr, ".rds"))
   
   # Obtaining occurrence points  -------------------------------------------------------------------
   # Obtain Occurrence points for year 100 (year before cc)
   pop_year100 <- subset(pop, pop$Year == 100)
   pop_year100 <- pop_year100 %>% dplyr::select(-c(RepSeason, Species, Year))
-  
+
   # Extract coordinates where the number of individuals was above 1
   occ_year100 <- subset(pop_year100, pop_year100$NInd >= 1)
+
+  # change coordinates to match climate coordinates (move coordinate to center and multiply with the resolution)
+  occ_year100$x <- (occ_year100$x + 0.5) * 1000
+  occ_year100$y <- (occ_year100$y + 0.5) * 1000
   colnames(occ_year100)[colnames(occ_year100) == "x"] <- "X"
   colnames(occ_year100)[colnames(occ_year100) == "y"] <- "Y"
-  
+
   # store the occurrence points of every replicated run in a separate element of a list
   ls_Occ<- split(occ_year100, occ_year100$Rep)
   saveRDS(ls_Occ, paste0(sdm_dir, "data/occurrences/Occ_list_Batch", BatchNum, "_Sim", rep_nr, ".rds"))
@@ -167,7 +171,7 @@ foreach(sim_nr=46:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "t
   text(x = 0.45, y = 0.1, paste0("Land ", rep_nr),
        cex = 1, col = "black", family="serif", font=2, adj=0.5)
   par(mfrow=c(1,1))
-  
+
   dev.off()
   
   # Plot occurrences in landscape under cc -----------------------------------------------
@@ -180,22 +184,23 @@ foreach(sim_nr=46:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "t
   # extract occurrences
   occ_short <- subset(pop_short, pop_short$NInd >= 1)
   #change column names
+  occ_short$x <- (occ_short$x + 0.5) * 1000
+  occ_short$y <- (occ_short$y + 0.5) * 1000
   colnames(occ_short)[colnames(occ_short) == "x"] <- "X"
   colnames(occ_short)[colnames(occ_short) == "y"] <- "Y"
   # extract only the first replication
   occ_Rep0 <- subset(occ_short, occ_short$Rep == 0)
+  
   # Plot the occurrences under climate change
   for (i in 0:89) {
     tmp <-terra::rast(paste0(sim_dir, "Inputs/land", rep_nr, "_optima",  optima, "_breadth", breadth, "_ccYear", i, ".asc"))
     occ_sub <- subset(occ_Rep0, occ_Rep0$Year == i+100)
-    occ_sub$X <- occ_sub$X * 1000
-    occ_sub$Y <- occ_sub$Y * 1000
     m <- terra::vect(occ_sub, geom = c("X", "Y"))
     if(length(m) >0){
-      plot(tmp)
-      plot(m, add = T)
+      terra::plot(tmp, col = rev(grDevices::terrain.colors(50)))
+      terra::plot(m, add = T)
     } else {
-      plot(tmp)
+      terra::plot(tmp, col = rev(grDevices::terrain.colors(50)))
     }
   }
   dev.off()
