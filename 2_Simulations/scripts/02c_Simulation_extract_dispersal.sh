017b376163b1f0a213b65d37f4b3abb057fb74aa
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2024-11-28 11:16:50 +0100

    initial commit

---
+++ 2_Simulations/scripts/02c_Simulation_extract_dispersal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=simulations_dispersal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=keuth@uni-potsdam.de
+#SBATCH --cpus-per-task=1
+#SBATCH --time=96:00:00
+#SBATCH --mem=100gb
+#SBATCH --nodelist=ecoc9
+
+cd ${SLURM_SUBMIT_DIR}
+R CMD BATCH ./02c_Simulation_extract_dispersal.R ./output-file-simlations_dispersal.Rout
