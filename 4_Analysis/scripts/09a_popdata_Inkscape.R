5553b57bf8ce9ccc1814adac1e3d32fb164e4d52
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2025-01-27 16:31:55 +0100

    Commit on script

--- 4_Analysis/scripts/09a_popdata_Inkscape.R
+++ 4_Analysis/scripts/09a_popdata_Inkscape.R
@@ -7,18 +7,24 @@ library(foreach)
 #define file path
 sim_dir <- file.path("/import/ecoc9z/data-zurell/keuth/SDM_Extinctions/02_Simulations/")
 
+# start cluster
 ncores <- 16
 cl <- makeCluster(ncores)
 registerDoParallel(cl)
 
 foreach(BatchNum=1:16, .packages = c("data.table")) %dopar% {
   
+  #read in pop data
   pop <- fread(paste0(sim_dir, "Outputs/Batch", BatchNum, "_Sim1_Land1_Pop.txt"))
   
+  # extract replicate run
   pop <- subset(pop, pop$Rep == 3)
+  
+  #correct coordinates
   pop$x <- (pop$x + 0.5) * 1000
   pop$y <- (pop$y + 0.5) * 1000
   
+  #save data set
   save(pop, file = paste0(sim_dir, "Outputs/Batch", BatchNum, "_Sim1_Land1_Pop_Rep4.Rdata"))
 }
 
--- 4_Analysis/scripts/09b_Inkscape_plots.R
+++ 4_Analysis/scripts/09b_Inkscape_plots.R
@@ -1,4 +1,5 @@
 # Create the abundance and prediction plots on the cluster to avoid large data transfer
+# PLots are done for all species for landscape 1 and replicate run 3
 
 #Load packages
 
@@ -10,7 +11,7 @@ sim_dir <- file.path("/import/ecoc9z/data-zurell/keuth/SDM_Extinctions/02_Simula
 sdm_dir <- file.path("/import/ecoc9z/data-zurell/keuth/SDM_Extinctions/03_SDMs/")
 
 for(BatchNum in 1:16){
-  # Load data
+  # Load data (current prediction of SDMs)
   load(paste0(sdm_dir, "predictions/Predictions_curr_Batch", BatchNum, "_Sim1_Replication4.RData"))
   
   #Rasterize data
@@ -21,7 +22,7 @@ for(BatchNum in 1:16){
   plot(r_current, legend = F, axes = F, col = rev(terrain.colors(100)), range = c(0,1))
   dev.off()
   
-  # Load data
+  # Load data (future predictions of SDMs)
   load(paste0(sdm_dir, "predictions/Predictions_fut_Batch", BatchNum, "_Sim1_Replication4.RData"))
   
   # extract two different years (I am not sure which one I should select)
@@ -32,7 +33,7 @@ for(BatchNum in 1:16){
   r_fut_20 <- terra::rast(fut_preds_20[,1:3])
   r_fut_30 <- terra::rast(fut_preds_30[,1:3])
   
-  #plot the results
+  #plot the results of both years
   pdf(paste0(sdm_dir, "plots/Inkscape_maps/SDM_predictions_map_future20_Batch", BatchNum, "_Rep4_Land1.pdf"))
   plot(r_fut_20, legend = F, axes = F, col = rev(terrain.colors(100)), range = c(0,1))
   dev.off()
@@ -43,25 +44,33 @@ for(BatchNum in 1:16){
   
   # Abundance plots of the simulation
 
-  # For the first species
+  # load data of abundances (the before extracted one replication of the pop data set)
   load(paste0(sim_dir, "Outputs/Batch", BatchNum, "_Sim1_Land1_Pop_Rep4.Rdata"))
 
-  # Abundance plot for year 0
+  # Extract values for year 0
   pop_current <- subset(pop, pop$Year == 100)
+  #correct spatial extent
   pop_current_full <- merge(ens_preds[,c(1:2)], pop_current[,c(4:5,7)], by = c("x","y"), all.x = T)
+  #correct NAs to 0 (otherwise the plotting looks odd and I know that all NAs are 0s)
   pop_current_full[which(is.na(pop_current_full$NInd)),"NInd"] <- 0
+  #rasterize data set
   r_abu_current <- rast(as.data.frame(pop_current_full[,c(1:3)]))
 
+  # Plot abundances for current climatic conditions
   pdf(paste0(sdm_dir, "plots/Inkscape_maps/Abundances_map_current_Batch", BatchNum, "_Rep4_Land1.pdf"))
   plot(r_abu_current, axes = F, range = c(0,11), legend = F, smooth = T, col = c("#F2F2F2", rev(brewer.pal(n = 11, name = "Spectral"))))
   dev.off()
   
-  # Abundance plot for year 20
+  # Extract values for year 20
   pop_fut_20 <- subset(pop, pop$Year == 120)
+  #correct spatial extent
   pop_fut_20_full <- merge(ens_preds[,c(1:2)], pop_fut_20[,c(4:5,7)], by = c("x","y"), all.x = T)
+  #correct NAs to 0 (otherwise the plotting looks odd and I know that all NAs are 0s)
   pop_fut_20_full[which(is.na(pop_fut_20_full$NInd)),"NInd"] <- 0
+  #rasterize data set
   r_abu_fut_20 <- rast(as.data.frame(pop_fut_20_full[,c(1:3)]))
   
+  # if there are no abundances in the data sets I set one color for the whole map otherwise I use a palette
   if(length(pop_fut_20_full$NInd) == 1){
     pdf(paste0(sdm_dir, "plots/Inkscape_maps/Abundances_map_future20_Batch", BatchNum, "_Rep4_Land1.pdf"))
     plot(r_abu_fut_20, axes = F, range = c(0,11), legend = F, smooth = T, col = c("#F2F2F2"))
@@ -72,12 +81,16 @@ for(BatchNum in 1:16){
     dev.off()
   }
   
-  # Abundance plot for year 30
+  # Extract values for year 30
   pop_fut_30 <- subset(pop, pop$Year == 130)
+  #correct spatial extent
   pop_fut_30_full <- merge(ens_preds[,c(1:2)], pop_fut_30[,c(4:5,7)], by = c("x","y"), all.x = T)
+  #correct NAs to 0 (otherwise the plotting looks odd and I know that all NAs are 0s)
   pop_fut_30_full[which(is.na(pop_fut_30_full$NInd)),"NInd"] <- 0
+  #rasterize data set
   r_abu_fut_30 <- rast(as.data.frame(pop_fut_30_full[,c(1:3)]))
   
+  # if there are no abundances in the data sets I set one color for the whole map otherwise I use a palette
   if(length(pop_fut_30_full$NInd) == 1){
     pdf(paste0(sdm_dir, "plots/Inkscape_maps/Abundances_map_future30_Batch", BatchNum, "_Rep4_Land1.pdf"))
     plot(r_abu_fut_30, axes = F, range = c(0,11), legend = F, smooth = T, col = c("#F2F2F2"))
