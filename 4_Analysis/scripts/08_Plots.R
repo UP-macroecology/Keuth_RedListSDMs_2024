f55ce42d7a92f1a3f1c66c191b522fe927747428
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2025-01-27 16:23:29 +0100

    updated plots and extracted data

--- 4_Analysis/scripts/08_Plots.R
+++ 4_Analysis/scripts/08_Plots.R
@@ -87,7 +87,7 @@ data_adapted_long <- do.call(rbind, data_adapted)
 data_adapted_long$predictions <- NA
 data_adapted_long <- as.data.frame(data_adapted_long)
 
-save(data_adapted_long, file="4_Analysis/data/raw_data_longformat.RData")
+#save(data_adapted_long, file="4_Analysis/data/raw_data_longformat.RData")
 
 # Plot the results -----
 
@@ -204,7 +204,7 @@ ggplot(data_optima, aes(x=hs_loss, y = predictions, col = land, linetype = optim
   labs(colour = "Landscape", linetype = NULL)+
   guides(linetype = guide_legend(order = 1))
 
-grid.arrange(p_pos, shared_legend, ncol=2, nrow = 1, widths = c(10,1))
+#grid.arrange(p_pos, shared_legend, ncol=2, nrow = 1, widths = c(10,1))
 
 
 p_pos <- ggplot(data_optima, aes(x=hs_loss, y = predictions, col = land, linetype = optima))+
@@ -294,6 +294,36 @@ grid.arrange(arrangeGrob(p_pos,p_breadth, p_rmax, p_dispersal, nrow=2, ncol = 2,
 IUCN_classification$optima <- as.character(IUCN_classification$optima)
 IUCN_classification[which(IUCN_classification$optima == "marginal"), "optima"] <- "range-contracting"
 IUCN_classification[which(IUCN_classification$optima == "central"), "optima"] <- "range-shifting"
+
+p_pos1 <- ggplot(IUCN_classification, aes(x = optima, y = VU_HS))+
+  geom_boxplot(width = 0.06, fill = "#EE2C2C", position = position_nudge(x = -0.42))+
+  geom_boxplot(aes(x = optima, y = VU_Ext), position = position_nudge(x = -0.24), width = 0.06, fill = "#1C86EE")+
+  geom_boxplot(aes(x = optima, y = VU_Pop), position = position_nudge(x = - 0.33), width = 0.06, fill = "orange")+
+  geom_boxplot(aes(x = optima, y = EN_HS), width = 0.06, fill = "#EE2C2C", position = position_nudge(x = -0.09))+
+  geom_boxplot(aes(x = optima, y = EN_Ext), position = position_nudge(x = 0.09), width = 0.06, fill = "#1C86EE")+
+  geom_boxplot(aes(x = optima, y = EN_Pop), position = position_nudge(x = 0), width = 0.06, fill = "orange")+
+  geom_boxplot(aes(x = optima, y = CR_HS), width = 0.06, fill = "#EE2C2C", position = position_nudge(x = 0.24))+
+  geom_boxplot(aes(x = optima, y = CR_Ext), position = position_nudge(x = 0.43), width = 0.06, fill = "#1C86EE")+
+  geom_boxplot(aes(x = optima, y = CR_Pop), position = position_nudge(x = 0.33), width = 0.06, fill = "orange")+
+  geom_vline(xintercept = 1.5)+
+  geom_vline(xintercept = 1.16, linetype = "dashed", color = "lightgrey")+
+  geom_vline(xintercept = 0.83, linetype = "dashed", color = "lightgrey")+
+  geom_vline(xintercept = 2.16, linetype = "dashed", color = "lightgrey")+
+  geom_vline(xintercept = 1.83, linetype = "dashed", color = "lightgrey")+
+  annotate(geom="text", x=0.655, y=59, label="VU", color="black", size = 8)+
+  annotate(geom="text", x=0.995, y=59, label="EN", color="black", size = 8)+
+  annotate(geom="text", x=1.325, y=59, label="CR", color="black", size = 8)+
+  annotate(geom="text", x=1.685, y=59, label="VU", color="black", size = 8)+
+  annotate(geom="text", x=1.995, y=59, label="EN", color="black", size = 8)+
+  annotate(geom="text", x=2.335, y=59, label="CR", color="black", size = 8)+
+  scale_x_discrete(expand = c(0.25, 0.25)) +
+  ggtitle("Range dynamics")+
+  xlab("")+
+  ylim(c(0,60))+
+  theme(axis.title.x = element_blank(), axis.text = element_text(size = 22),
+        axis.title = element_text(size = 24), legend.position = "", plot.title = element_text(size = 24, face = "italic"), 
+        panel.grid = element_blank(), panel.background = element_rect(fill = "white"), panel.border = element_rect(colour = "black", fill = NA, linewidth = 1))+
+  ylab("Classification timepoint [years]")
 
 p_pos <- ggplot(IUCN_classification, aes(x = optima, y = VU_HS))+
   geom_boxplot(width = 0.06, fill = "#EE2C2C", position = position_nudge(x = -0.42))+
@@ -420,7 +450,7 @@ legend <- ggplot(IUCN_classification, aes(x = BatchNum, y = VU_Pop, fill = "Popu
   geom_boxplot(aes(x = BatchNum, y = VU_HS, fill = "Extinction probability (E)"), position = position_nudge(x = 0.25), width = 0.2)+
   theme_bw()+
   theme(axis.title.x = element_blank(), axis.text = element_text(size = 18),
-        axis.title = element_text(size = 20), plot.title = element_text(size = 25, face = "bold"), legend.title = element_blank(), legend.text = element_text(size = 24), legend.key.size = unit(1.5, "cm"),
+        axis.title = element_text(size = 20), plot.title = element_text(size = 25, face = "bold"), legend.title = element_blank(), legend.text = element_text(size = 25), legend.key.size = unit(1.5, "cm"),
         legend.position = "bottom")+
   ylab("Timepoint of classification")+
   scale_fill_manual(values= colors, breaks = c("Habitat suitability (A3)", "Population size (A3)", "Extinction probability (E)"))
@@ -429,7 +459,7 @@ shared_legend <- extract_legend(legend)
 
 #Plot large grid
 grid.arrange(arrangeGrob(p_pos,p_breadth, p_rmax, p_disp, nrow=2, ncol = 2, heights = c(8,8), widths = c(1,1)), shared_legend, nrow=2, ncol = 1, heights = c(10,1))
-grid.arrange(p_pos, shared_legend, nrow=2, ncol = 1, heights = c(10,1))
+grid.arrange(p_pos1, shared_legend, nrow=2, ncol = 1, heights = c(10,1))
 
 
 # plot of HS, population size and extinction probability over time --------
@@ -669,11 +699,11 @@ legend <- ggplot(data_mean[[16]], aes(x = Year, y = meanHS, color = "relative Ha
   ylab("")+
   theme_bw()+
   theme(axis.text = element_text(size = 18), axis.title = element_text(size = 18), legend.position = "bottom", , legend.title = element_blank(), 
-        legend.text = element_text(size = 15), legend.key.size = unit(1, "cm"))+
-  scale_color_manual(values= colors)
+        legend.text = element_text(size = 20), legend.key.size = unit(1.5, "cm"))+
+  scale_color_manual(values= colors, breaks = c("relative Habitat suitability", "relative Population size", "Extinction probability"))
 
 shared_legend <- extract_legend(legend)
 
 #Plot the large grid
-grid.arrange(arrangeGrob(t0, t_cn, t_cw, t_wna, t_ww, t_ss, p1,p3,p2,p4, t_sl, p9,p11,p10,p12, t_fs, p5,p7,p6,p8, t_fl, p13,p15,p14,p16, nrow = 5, ncol = 5, heights= c(1,3.8,4,4,4.2), widths = c(1.5,5,5,5,5)),
+grid.arrange(arrangeGrob(t0, t_cn, t_cw, t_wna, t_ww, t_ss, p1,p3,p2,p4, t_sl, p9,p11,p10,p12, t_fs, p5,p7,p6,p8, t_fl, p13,p15,p14,p16, nrow = 5, ncol = 5, heights= c(1,3.8,4,4,4.2), widths = c(1.8,5,5,5,5)),
              t0,shared_legend, nrow = 2, ncol = 2, heights = c(11.2, 0.8), widths = c(11.7,0.3))
