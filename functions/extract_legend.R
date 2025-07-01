42473308b7a5a1ed6b2f6fcffd3d7dee41768d7c
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2025-07-01 16:16:29 +0200

    Remove functions from tracking

--- functions/extract_legend.R
+++
@@ -1,8 +0,0 @@
-# Function to extract the legend from a ggplot object
-
-extract_legend <- function(my_ggp) {
-  step1 <- ggplot_gtable(ggplot_build(my_ggp))
-  step2 <- which(sapply(step1$grobs, function(x) x$name) == "guide-box")
-  step3 <- step1$grobs[[step2]]
-  return(step3)
-}
