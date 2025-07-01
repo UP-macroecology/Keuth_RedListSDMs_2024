28b87b14bb382092d120589ba9dbf471b32e9415
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2025-07-01 16:48:35 +0200

    renamed

--- scripts/03_Simulations.R
+++ scripts/03_Simulations.R
@@ -1,14 +1,24 @@
-# RangeShifter simulations for the 16 different scenarios for each landscape replica
+# Red List criteria underestimate climate-related extinction risk of range-shifting species
+
+
+#-------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------- #
+#                         03. Run RangeShifter Simulations                   #
+# -------------------------------------------------------------------------- #
+
+
+#-------------------------------------------------------------------------------
+
+# RangeShifter simulations for the 16 different virtual species for each landscape replica
+# Here the trait values growth rate and dispersal are adapted
 
 # The different values for the different scenarios are
-# niche position: cold (0.27), warm (0.5)
+# niche position: marginal (0.27), central (0.5)
 # niche breadth: narrow (0.025), wide (0.035)
-# reproduction: slow (3), fast (5)
+# growth rate: slow (3), fast (5)
 # dispersal: short (5000), long (15 000, 250 000, 0.95)
 
-#define file path
-sim_dir <- file.path("/import/ecoc9z/data-zurell/keuth/SDM_Extinctions/02_Simulations/")
-sdm_dir <- file.path("/import/ecoc9z/data-zurell/keuth/SDM_Extinctions/03_SDMs/")
 
 # Load packages
 require(RangeShiftR)
@@ -22,19 +32,25 @@ library(scales)
 library(data.table)
 library(terra)
 
-# Function for calculating extinction probability
-source("/import/ecoc9z/data-zurell/keuth/SDM_Extinctions/02_Simulations/Functions/Extinction_probability.R")
+# define file paths
+home_folder <- file.path("/import/ecoc9z/data-zurell/keuth/SDM_Extinctions/") #needs to be adjusted based on own folder structure
+sim_dir <- file.path(paste0(home_folder, "Simulations/"))
+sdm_dir <- file.path(paste0(home_folder, "output_data/SDMs/"))
+
+# Loading functions
+source("scripts/00_functions.R")
 
 # create data frame with all parameter combinations
 land_rep <- 1:3
-optima <- c(0.27, 0.5)
+position <- c(0.27, 0.5)
 breadth <- c(0.045, 0.055)
 rmax <- c(3, 5)
 dispersal <- c(5000, 15000)
 
-sims <- expand.grid(land_rep = land_rep, optima = optima, breadth = breadth, rmax = rmax, dispersal = dispersal)
+sims <- expand.grid(land_rep = land_rep, position = position, breadth = breadth, rmax = rmax, dispersal = dispersal)
 sims$BatchNum <- rep(1:16, each = 3)
 
+#prepare script to run on HPC
 ncores <- 10
 cl <- makeCluster(ncores)
 registerDoParallel(cl)
@@ -42,7 +58,7 @@ registerDoParallel(cl)
 foreach(sim_nr=1:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "tibble", "scales", "ggplot2", "gridExtra", "terra")) %dopar% {
   # Extract parameter values
   rep_nr <- sims[sim_nr,]$land_rep
-  optima <- sims[sim_nr,]$optima
+  position <- sims[sim_nr,]$position
   breadth <- sims[sim_nr,]$breadth
   rmax <- sims[sim_nr,]$rmax
   dispersal <- sims[sim_nr,]$dispersal
@@ -55,7 +71,7 @@ foreach(sim_nr=1:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "ti
   # Set dynamic landscape parameters
   landnames <- c()
   for (i in 0:89){
-    landnames <- append(landnames, paste0("land", rep_nr, "_optima",  optima, "_breadth", breadth, "_ccYear", i, ".asc"))
+    landnames <- append(landnames, paste0("land", rep_nr, "_position",  position, "_breadth", breadth, "_ccYear", i, ".asc"))
   }
       
     #create vector for the years (landscapes start changing at year 101)
@@ -107,7 +123,8 @@ foreach(sim_nr=1:nrow(sims), .packages = c("RangeShiftR", "dplyr", "scales", "ti
                       Replicates = RepNb,
                       Years = spinup + sim_years,
                       OutIntPop = 1,
-                      OutIntOcc = 1)
+                      OutIntOcc = 1,
+                      OutIntInd = 1)
       
     s <- RSsim(batchnum = BatchNum, land = land, demog = demo, dispersal = disp, simul = sim, init = init)
       
